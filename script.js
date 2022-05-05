f21fa1059d9596d7fe9a1b0b23e0ef6808709b93
Alan Tsarakov <alantsarakov@yandex.ru>
2022-05-05 21:58:37 +0300

    hold fn

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -28,7 +30,13 @@ btnRoll.addEventListener('click', function () {
 
   if (diceNum !== 1) {
     currentScore += diceNum;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
